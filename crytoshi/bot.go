0d3efb8e2ab67f7bd7c5337ed1e3604f2c7e7d77
Nekodoll <37606008+Nekodoll@users.noreply.github.com>
2022-10-22 14:46:49 +0700

    Create bot.go

---
+++ crytoshi/bot.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+)
+
+func main() {
+
+	getURL := "https://cryptoshi.club/home.php?LTC=1&DOGE=1&DGB=1&TRX=1&USDT=1&BCH=1&DASH=1&FEY=1&ZEC=1&SOL=1&redirect=1"
+	data := url.Values{}
+	getHeader := "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"
+	client := &http.Client{}
+	request, _ := http.NewRequest(http.MethodGet, getURL, strings.NewReader(data.Encode()))
+	request.Header.Add("User-Agent", getHeader)
+	request.Header.Add("content-type", "application/x-www-form-urlencoded")
+	request.Header.Add("Cookie", "PHPSESSID=6hrr260p5ul468n1q6u1nnnkd3")
+	for {
+		resp, _ := client.Do(request)
+		fmt.Println(resp.StatusCode)
+		time.Sleep(60 * time.Second)
+	}
+
+}
