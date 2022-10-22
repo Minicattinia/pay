053b1ceb9d1cefcbce8a2403c73ef71a15add547
Nekodoll <37606008+Nekodoll@users.noreply.github.com>
2022-10-22 14:52:18 +0700

    Update bot.go

--- crytoshi/bot.go
+++ crytoshi/bot.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-
+	colorGreen := "\033[32m"
 	getURL := "https://cryptoshi.club/home.php?LTC=1&DOGE=1&DGB=1&TRX=1&USDT=1&BCH=1&DASH=1&FEY=1&ZEC=1&SOL=1&redirect=1"
 	data := url.Values{}
 	getHeader := "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"
@@ -19,8 +19,10 @@ func main() {
 	request.Header.Add("content-type", "application/x-www-form-urlencoded")
 	request.Header.Add("Cookie", "PHPSESSID=6hrr260p5ul468n1q6u1nnnkd3")
 	for {
-		resp, _ := client.Do(request)
-		fmt.Println(resp.StatusCode)
+		resp, err:= client.Do(request)
+		if err != nil {
+		fmt.Println(string(colorGreen), "success Claim")
+		}
 		time.Sleep(60 * time.Second)
 	}
 
