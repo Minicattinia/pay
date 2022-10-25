1540777e4a2e417975e0b528a3db6a07206bb313
Nekodoll <37606008+Nekodoll@users.noreply.github.com>
2022-10-25 14:22:07 +0700

    Create bot.go

---
+++ Auto_Claim/bot.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+)
+
+func claim() {
+	var getURL, getHeader, getCookie string
+	var getDelay int
+	colorGreen := "\033[32m"
+	data := url.Values{}
+	getHeader = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"
+	fmt.Print("Insert web site Url")
+	fmt.Scan(&getURL)
+	fmt.Print("Insert web Cookie")
+	fmt.Scan(&getCookie)
+	fmt.Print("Insert Claim Dealey Minute")
+	fmt.Scan(&getDelay)
+	client := &http.Client{}
+	request, _ := http.NewRequest(http.MethodGet, getURL, strings.NewReader(data.Encode()))
+	request.Header.Add("User-Agent", getHeader)
+	request.Header.Add("content-type", "application/x-www-form-urlencoded")
+	request.Header.Add("Cookie", getCookie)
+	for {
+		resp, _ := client.Do(request)
+		if resp.StatusCode == 200 {
+			fmt.Println(string(colorGreen), " Success Claim")
+		}
+		time.Sleep(time.Duration(getDelay) * 60 * time.Second)
+	}
+
+}
